bd857fd2f7996bd3d84d54366fa74adf015d04e8
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-17 18:40:23 +0000

    python manager using mongodb

--- project2/youtube_manager.py
+++ project2/youtube_manager.py
@@ -9,3 +9,36 @@ db = client["ytmanager"]
 video_collections = db["videos"]
 
 print(video_collections)
+
+def main():
+    while True:
+        print("\n Youtube manager App")
+        print("1. List all youtube videos: ")
+        print("2. Add a youtube video: ")
+        print("3. Update a youtube video details: ")
+        print("4. Delete a youtube video: ")
+        print("5. Exit the app. ")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            list_videos() 
+        elif choice == "2":
+            name = input("Enter video name: ")
+            time = input("Enter video length: ")
+            add_video(name, time)
+        elif choice == "3":
+            video_id = int(input("Enter the video ID to update: "))
+            name = input("Enter new video name: ")
+            time = input("Enter new video length: ")
+            update_video(video_id, name, time)
+        elif choice == "4":
+            video_id = int(input("Enter the video ID to update: "))
+            delete_video(video_id)
+        elif choice == "5":
+            break
+        else:
+            print("Invalid Choice")
+
+if __name__ == "__main__":
+    main()
