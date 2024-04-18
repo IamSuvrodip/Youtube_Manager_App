7dd7f43a152012a0293e2f31ec8063b48978dcf1
Suvrodip Chakroborty <113452951+IamSuvrodip@users.noreply.github.com>
2024-04-18 09:00:35 +0000

    routing_python_using_fastapi

--- Routing_Python/routing_python.py
+++ Routing_Python/routing_python.py
@@ -3,3 +3,26 @@
 
 # pip install fastapi
 # pip imstall fastapi uvicorn
+
+# for uv package all packages-speed very very fast working
+
+from fastapi import FastAPI, UploadFile
+
+app = FastAPI()
+
+@app.get('/')
+def home():
+    return {'data':'welcome to home page'}
+
+@app.get('/contact')
+def contact():
+    return {'data':'welcome to contact page'}
+
+# pip install python-multipart
+@app.post('/upload')
+def handleImage(files: list[UploadFile]):
+    print(files)
+    return {'status': 'got the file'}
+
+import uvicorn
+uvicorn.run(app)
